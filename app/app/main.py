39ad05116f281f90963f7ab8f1630803bd0123e5
zapads18-creator <zapads18@gmail.com>
2026-08-05 23:00:36 +0500

    Update main.py

--- app/app/main.py
+++ app/app/main.py
@@ -1,1 +1,17 @@
-print("BronlaDacha ishga tushdi!")
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+import asyncio
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def main():
+    bot = Bot(token=TOKEN, default_parse_mode=ParseMode.HTML)
+    dp = Dispatcher()
+
+    print("✅ BronlaDacha ishga tushdi.")
+
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
