dfaab5d94c5aee0eced2cbd027586657bafac297
zapads18-creator <zapads18@gmail.com>
2026-08-05 15:34:58 +0500

    Add files via upload

---
+++ regions.py
@@ -0,0 +1,76 @@
+# O'zbekiston viloyatlari va tumanlari
+
+REGIONS = {
+    "Qoraqalpog'iston Respublikasi": [
+        "Nukus shahri", "Amudaryo", "Beruniy", "Chimboy", "Ellikqal'a", "Kegeyli",
+        "Mo'ynoq", "Nukus tumani", "Qanlikol", "Qo'ng'irot", "Qorao'zak",
+        "Shumanay", "Taxtako'pir", "To'rtko'l", "Xo'jayli",
+    ],
+    "Andijon viloyati": [
+        "Andijon shahri", "Andijon tumani", "Asaka", "Baliqchi", "Bo'ston",
+        "Buloqboshi", "Izboskan", "Jalaquduq", "Xo'jaobod", "Marhamat",
+        "Oltinko'l", "Paxtaobod", "Qo'rg'ontepa", "Shahrixon", "Ulug'nor",
+        "Xonobod shahri",
+    ],
+    "Buxoro viloyati": [
+        "Buxoro shahri", "Buxoro tumani", "Vobkent", "G'ijduvon", "Jondor",
+        "Kogon shahri", "Kogon tumani", "Olot", "Peshku", "Qorako'l",
+        "Qorovulbozor", "Romitan", "Shofirkon",
+    ],
+    "Farg'ona viloyati": [
+        "Farg'ona shahri", "Marg'ilon shahri", "Qo'qon shahri", "Farg'ona tumani",
+        "Bag'dod", "Beshariq", "Buvayda", "Dang'ara", "Furqat", "Oltiariq",
+        "Quva", "Qo'shtepa", "Rishton", "So'x", "Toshloq", "Uchko'prik",
+        "O'zbekiston tumani", "Yozyovon",
+    ],
+    "Jizzax viloyati": [
+        "Jizzax shahri", "Arnasoy", "Baxmal", "Do'stlik", "Forish", "G'allaorol",
+        "Mirzachoʻl", "Paxtakor", "Sharof Rashidov", "Yangiobod", "Zafarobod",
+        "Zarbdor", "Zomin",
+    ],
+    "Xorazm viloyati": [
+        "Urganch shahri", "Xiva shahri", "Bog'ot", "Gurlan", "Xazorasp", "Xonqa",
+        "Qo'shko'pir", "Shovot", "Urganch tumani", "Yangiariq", "Yangibozor",
+    ],
+    "Namangan viloyati": [
+        "Namangan shahri", "Chortoq", "Chust", "Kosonsoy", "Mingbuloq",
+        "Namangan tumani", "Norin", "Pop", "To'raqo'rg'on", "Uchqo'rg'on",
+        "Uychi", "Yangiqo'rg'on",
+    ],
+    "Navoiy viloyati": [
+        "Navoiy shahri", "Zarafshon shahri", "Karmana", "Konimex", "Navbahor",
+        "Nurota", "Qiziltepa", "Tomdi", "Uchquduq", "Xatirchi",
+    ],
+    "Qashqadaryo viloyati": [
+        "Qarshi shahri", "Shahrisabz shahri", "Qarshi tumani", "Chiroqchi",
+        "Dehqonobod", "G'uzor", "Kasbi", "Kitob", "Koson", "Mirishkor",
+        "Muborak", "Nishon", "Qamashi", "Shahrisabz tumani", "Yakkabog'",
+    ],
+    "Samarqand viloyati": [
+        "Samarqand shahri", "Kattaqo'rg'on shahri", "Samarqand tumani",
+        "Bulung'ur", "Ishtixon", "Jomboy", "Kattaqo'rg'on tumani", "Narpay",
+        "Nurobod", "Oqdaryo", "Payariq", "Pastdarg'om", "Paxtachi", "Toyloq",
+        "Urgut",
+    ],
+    "Sirdaryo viloyati": [
+        "Guliston shahri", "Yangiyer shahri", "Shirin shahri", "Guliston tumani",
+        "Boyovut", "Mirzaobod", "Oqoltin", "Sardoba", "Sayxunobod",
+        "Sirdaryo tumani", "Xovos",
+    ],
+    "Surxondaryo viloyati": [
+        "Termiz shahri", "Termiz tumani", "Angor", "Bandixon", "Boysun",
+        "Denov", "Jarqo'rg'on", "Muzrabot", "Oltinsoy", "Qiziriq",
+        "Qumqo'rg'on", "Sariosiyo", "Sherobod", "Sho'rchi", "Uzun",
+    ],
+    "Toshkent viloyati": [
+        "Nurafshon shahri", "Angren shahri", "Chirchiq shahri", "Olmaliq shahri",
+        "Bekobod", "Bo'ka", "Bo'stonliq", "Chinoz", "Ohangaron", "Oqqo'rg'on",
+        "Parkent", "Piskent", "Qibray", "Quyi Chirchiq", "O'rta Chirchiq",
+        "Yangiyo'l", "Yuqori Chirchiq", "Zangiota",
+    ],
+    "Toshkent shahri": [
+        "Bektemir", "Chilonzor", "Mirobod", "Mirzo Ulug'bek", "Olmazor",
+        "Sergeli", "Shayxontohur", "Uchtepa", "Yakkasaroy", "Yangihayot",
+        "Yashnobod", "Yunusobod",
+    ],
+}
