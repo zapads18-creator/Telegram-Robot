def25524402d69902e7991e07f96f7d6a621bb4b
zapads18-creator <zapads18@gmail.com>
2026-08-05 23:16:56 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,27 @@
 import asyncio
+import os
+
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
+
 
 async def main():
+    token = os.getenv("BOT_TOKEN")
+
+    bot = Bot(
+        token=token,
+        default=DefaultBotProperties(
+            parse_mode=ParseMode.HTML
+        )
+    )
+
+    dp = Dispatcher()
+
     print("🚀 TezDacha ishga tushdi")
+
+    await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
