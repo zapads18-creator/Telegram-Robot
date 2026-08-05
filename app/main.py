0d79cc709cdd106a335fe63146351dc2aa1015f6
zapads18-creator <zapads18@gmail.com>
2026-08-05 23:54:19 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,22 +1,23 @@
 import asyncio
-import os
 
 from aiogram import Bot, Dispatcher
-from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
+from app.handlers.start import router
 
 
 async def main():
-    token = os.getenv("BOT_TOKEN")
-
     bot = Bot(
-        token=token,
+        token="BOT_TOKENINGIZ",
         default=DefaultBotProperties(
             parse_mode=ParseMode.HTML
         )
     )
 
     dp = Dispatcher()
+
+    dp.include_router(router)
 
     print("🚀 TezDacha ishga tushdi")
 
